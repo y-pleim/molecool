3c078b6f5844cfecdc503eb72b6d10cf05027f17
Yannick Pleimling <yannickp@vt.edu>
2022-02-01 21:05:22 -0500

    changed functions.py and filled data folder for workshop pt.4

--- molecool2/functions.py
+++ molecool2/functions.py
@@ -1,57 +1,5 @@
 """Provide the primary functions."""
 
-
-def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format).
-
-    Replace this function and doc string for your own project.
-
-    Parameters
-    ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from.
-
-    Returns
-    -------
-    quote : str
-        Compiled string including quote and optional attribution.
-    """
-
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
-
-
-def zen(with_attribution=True):
-    quote = """Beautiful is better than ugly.
-    Explicit is better than implicit.
-    Simple is better than complex.
-    Complex is better than complicated.
-    Flat is better than nested.
-    Sparse is better than dense.
-    Readability counts.
-    Special cases aren't special enough to break the rules.
-    Although practicality beats purity.
-    Errors should never pass silently.
-    Unless explicitly silenced.
-    In the face of ambiguity, refuse the temptation to guess.
-    There should be one-- and preferably only one --obvious way to do it.
-    Although that way may not be obvious at first unless you're Dutch.
-    Now is better than never.
-    Although never is often better than *right* now.
-    If the implementation is hard to explain, it's a bad idea.
-    If the implementation is easy to explain, it may be a good idea.
-    Namespaces are one honking great idea -- let's do more of those!"""
-
-    if with_attribution:
-        quote += "\n\tTim Peters"
-
-    return quote
-
-
-import os
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -59,25 +7,66 @@ from mpl_toolkits.mplot3d import Axes3D
 
 
 def calculate_distance(rA, rB):
-    # This function calculates the distance between two points given as numpy arrays.
-    d = rA - rB
-    dist = np.linalg.norm(d)
-    return dist
+    """Calculate the distance between two points.
+
+    Parameters
+    ----------
+    rA, rB : np.ndarray
+        The coordinates of each point.
+
+    Returns
+    -------
+    distance : float
+        The distance between the two points.
+
+    Examples
+    --------
+    >>> r1 = np.array([0, 0, 0])
+    >>> r2 = np.array([0, 0.1, 0])
+    >>> calculate_distance(r1, r2)
+    0.1
+    """
+    distance_vector = rA - rB
+    distance = np.linalg.norm(distance_vector)
+
+    return distance
 
 
-def open_pdb(f_loc):
-    # This function reads in a pdb file and returns the atom names and coordinates.
-    with open(f_loc) as f:
+def open_pdb(file_location):
+    """Open a pdb file and retrieve atom symbols and coordinates.
+
+    The pdb file must specify the atom elements in the last column, and follow
+    the conventions outlined in the PDB format specification.
+
+    Parameters
+    ----------
+    file_location : str
+        The location of the pdb file.
+
+    Returns
+    -------
+    coords : np.array
+        The coordinates of the atoms in the pdb file.
+    symbols : list
+        The atomic symbols of the atoms in the pdb file.
+
+    """
+
+    with open(file_location) as f:
         data = f.readlines()
-    c = []
-    sym = []
-    for l in data:
-        if "ATOM" in l[0:6] or "HETATM" in l[0:6]:
-            sym.append(l[76:79].strip())
-            c2 = [float(x) for x in l[30:55].split()]
-            c.append(c2)
-    coords = np.array(c)
-    return sym, coords
+
+    coordinates = []
+    symbols = []
+
+    for line in data:
+        if "ATOM" in line[0:6] or "HETATM" in line[0:6]:
+            symbols.append(line[76:79].strip())
+            atom_coords = [float(x) for x in line[30:55].split()]
+            coordinates.append(atom_coords)
+
+    coords = np.array(coordinates)
+
+    return symbols, coords
 
 
 atomic_weights = {
@@ -106,6 +95,11 @@ def write_xyz(file_location, symbols, coordinates):
 
     # Write an xyz file given a file location, symbols, and coordinates.
     num_atoms = len(symbols)
+
+    if num_atoms != len(coordinates):
+        raise ValueError(
+            f"write_xyz : the number of symbols ({num_atoms}) and number of coordinates ({len(coordinates)}) must be the same to write xyz file!"
+        )
 
     with open(file_location, "w+") as f:
         f.write("{}\n".format(num_atoms))
@@ -228,6 +222,56 @@ atom_colors = {
     "Br": "#F4A460",
     "S": "yellow",
 }
+
+
+def canvas(with_attribution=True):
+    """
+    Placeholder function to show example docstring (NumPy format).
+
+    Replace this function and doc string for your own project.
+
+    Parameters
+    ----------
+    with_attribution : bool, Optional, default: True
+        Set whether or not to display who the quote is from.
+
+    Returns
+    -------
+    quote : str
+        Compiled string including quote and optional attribution.
+    """
+
+    quote = "The code is but a canvas to our imagination."
+    if with_attribution:
+        quote += "\n\t- Adapted from Henry David Thoreau"
+    return quote
+
+
+def zen(with_attribution=True):
+    quote = """Beautiful is better than ugly.
+    Explicit is better than implicit.
+    Simple is better than complex.
+    Complex is better than complicated.
+    Flat is better than nested.
+    Sparse is better than dense.
+    Readability counts.
+    Special cases aren't special enough to break the rules.
+    Although practicality beats purity.
+    Errors should never pass silently.
+    Unless explicitly silenced.
+    In the face of ambiguity, refuse the temptation to guess.
+    There should be one-- and preferably only one --obvious way to do it.
+    Although that way may not be obvious at first unless you're Dutch.
+    Now is better than never.
+    Although never is often better than *right* now.
+    If the implementation is hard to explain, it's a bad idea.
+    If the implementation is easy to explain, it may be a good idea.
+    Namespaces are one honking great idea -- let's do more of those!"""
+
+    if with_attribution:
+        quote += "\n\tTim Peters"
+
+    return quote
 
 
 if __name__ == "__main__":
